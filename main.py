5bd9319025de9ca45e2e75384035a42c2a7ef4d7
Gabriel Verissimo <gabrielverissimo735@gmail.com>
2025-07-11 23:10:47 -0300

    O código agora pode continuar depois de uma compra caso o usuário queira. Além disso, a função func_consulta pode fazer a diferança entre o "valor" - "valor do serviço".

--- main.py
+++ main.py
@@ -9,17 +9,35 @@ def main():
  
  try:
   valor = float(input("Digite o seu valor: "))
-  cliente = veterinario(valor, nome,servico)
+  cliente = veterinario(valor, nome, servico)
+  escolha = cliente.atendimento()
+ except ValueError:
+  print("Erro: Digite apenas números para o valor") 
+ 
+
+ while True: 
   print("")
   print("\n==========Resultado=========")
   print(f"O nome do cliente: {cliente.nome}",
         f"\nO valor: {cliente.valor}")
-   
-  escolha = cliente.atendimento()
-  if escolha: 
-   cliente.consulta(escolha)
- except ValueError:
-   print("\nError - Apenas números são aceitos -")
 
+  if escolha is None:
+    break
+  elif escolha: 
+    cliente.consulta(escolha)
+    if cliente.valor >= 0:
+     continuar = str(input("\nDeseja continuar o atendimento? (Sim/Não): ")).capitalize()
+     if continuar == 'Não':
+       print("-----Parando o Petshop------")
+       print("")
+       break
+     elif continuar == 'Sim':
+       print("------Continuando o Petshop-------")
+       print(f"\nO valor restante é: {cliente.valor}")   
+       print("")
+       escolha = cliente.atendimento()
+     else:
+       print("Apenas 'Sim' ou 'Não' são aceitos")
+       break       
 if __name__ == "__main__":
-  main()
+  main()      
--- pacote/consulta.py
+++ pacote/consulta.py
@@ -2,9 +2,11 @@
 def func_consulta(self,escolha):
            if self.valor < 0:
             print("\nO valor não pode estar negativado")
-           
+            
            nome_servico, preco = self.servico[escolha]  
            if self.valor >= preco:
-              print(f"O cliente {self.nome} pagou o serviço: {nome_servico}")
+              self.valor -= preco
+              print(f"\nO cliente {self.nome} pagou o serviço: {nome_servico}, ainda sobrou {self.valor} reais")
            else:
-              print(f"\nO cliente {self.nome} deu um valor insuficiente para o serviço {nome_servico}")  
+                print(f"\nO cliente {self.nome} não tem dinheiro suficiente para pagar o serviço: {nome_servico}")
+              
